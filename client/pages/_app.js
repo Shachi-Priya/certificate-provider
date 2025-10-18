06ba1fbb336f5a659ada0151c63dad84cdc3329b
Shachi Priya <shamil4621@gmail.com>
2025-10-18 20:49:10 +0530

    favico added

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,14 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/logo.png" type="image/png" />
+        <title>Royal Krishna Coaching</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,10 +1,13 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <link rel="icon" href="/logo.png" type="image/png" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
